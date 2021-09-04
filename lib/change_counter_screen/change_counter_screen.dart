e2895d7ec60d1c8fc38bf5bf96e6bdc752380f0f
Nikolay Shiryaev <deendeeps@list.ru>
2021-09-04 12:07:52 +0300

    Добавляет кнопки изменения счетчика и FAB для сохранения изменений

--- lib/change_counter_screen/change_counter_screen.dart
+++ lib/change_counter_screen/change_counter_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChangeCounterScreen extends StatelessWidget {
-
   ChangeCounterScreenBloc? _bloc;
   final Counter? counter;
 
@@ -13,13 +12,46 @@ class ChangeCounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     _bloc = BlocProvider.of<ChangeCounterScreenBloc>(context);
 
     return Scaffold(
-      body: Center(
-        child: Text(counter?.count.toString() ?? "0"),
+      body: counter != null
+            ? changeCounterWidget(counter!)
+            : counterIsNullWidget(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.check_rounded),
+        onPressed: () {}
       ),
+    );
+  }
+
+  Widget changeCounterWidget(Counter counter) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.remove_rounded),
+          ),
+          Text(
+            counter.count.toString(),
+            style: TextStyle(
+              fontSize: 20
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add_rounded),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget counterIsNullWidget() {
+    return Center(
+      child: Text("Counter is null"),
     );
   }
 }
