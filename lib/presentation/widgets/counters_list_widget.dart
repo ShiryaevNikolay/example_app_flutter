bee7f8d8d66ac732a89410acb08b06b4236a7cf6
Nikolay Shiryaev <deendeeps@list.ru>
2021-09-13 22:47:05 +0300

    Добавляет виджет с сообщением если пришли пустые данные

--- lib/presentation/widgets/counters_list_widget.dart
+++ lib/presentation/widgets/counters_list_widget.dart
@@ -1,5 +1,7 @@
+import 'package:example_app_flutter/data/bloc/main_screen/main_screen_state.dart';
 import 'package:example_app_flutter/domain/counter.dart';
 import 'package:example_app_flutter/presentation/widgets/counter_item_widget.dart';
+import 'package:example_app_flutter/presentation/widgets/empty_widget.dart';
 import 'package:flutter/widgets.dart';
 
 class CountersList extends StatelessWidget {
@@ -10,12 +12,16 @@ class CountersList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    if (_counters.isEmpty) {
+      return EmptyWidget(EmptyDataState.message);
+    } else {
+      return ListView.builder(
         itemCount: _counters.length,
         itemBuilder: (context, index) {
           Counter counter = _counters.elementAt(index);
 
           return CounterItem(counter, _onCounterTap);
         });
+    }
   }
 }
